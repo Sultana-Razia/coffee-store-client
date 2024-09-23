093192b16761394274128fe3c637fc5e4d6f3a86
Sultana Razia <sultanarazia483@gmail.com>
2024-09-23 17:45:04 +0600

    Delete a coffee

--- src/CoffeeCard.jsx
+++ src/CoffeeCard.jsx
@@ -1,8 +1,42 @@
 import React from 'react';
+import Swal from 'sweetalert2';
 
 const CoffeeCard = ({ coffee }) => {
 
-    const { name, quantity, supplier, category, taste, details, photo } = coffee;
+    const { _id, name, quantity, supplier, category, taste, details, photo } = coffee;
+
+    const handleDelete = _id => {
+        console.log(_id);
+
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                fetch(`http://localhost:5000/coffee/${_id}`, {
+                    method: 'DELETE',
+
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        if (data.deletedCount > 0) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your coffee has been deleted.",
+                                icon: "success"
+                            });
+                        }
+                    })
+            }
+        });
+    }
 
     return (
         <div>
@@ -22,7 +56,7 @@ const CoffeeCard = ({ coffee }) => {
                     <div className="btn-group btn-group-vertical space-y-4 flex flex-col">
                         <button className="btn">View</button>
                         <button className="btn">Edit</button>
-                        <button className="btn">X</button>
+                        <button onClick={() => handleDelete(_id)} className="btn">X</button>
                     </div>
                 </div>
             </div>
