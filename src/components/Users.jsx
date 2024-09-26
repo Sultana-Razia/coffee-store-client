dc9adbc64b376abbdf0a5524a7f21df2f6f90530
Sultana Razia <sultanarazia483@gmail.com>
2024-09-26 10:15:45 +0600

    Delete a user from client side

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 
 const Users = () => {
 
-    const loadedUsers = useLoaderData()
+    const loadedUsers = useLoaderData();
+    const [users, setUsers] = useState(loadedUsers);
+
+    const handleDelete = id => {
+
+        fetch(`http://localhost:5000/user/${id}`, {
+            method: 'DELETE'
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.deletedCount > 0) {
+                    console.log('deleted successfully');
+
+                    //remove the user from the UI
+                    const remainingUsers = users.filter(user => user._id !== id);
+                    setUsers(remainingUsers);
+                }
+            })
+    }
 
     return (
         <div className='w-[90%] mx-auto mt-20'>
@@ -16,23 +34,22 @@ const Users = () => {
                             <th></th>
                             <th>Email</th>
                             <th>CreatedAt</th>
+                            <th>Last Logged In</th>
                             <th>Action</th>
                         </tr>
                     </thead>
                     <tbody>
-                        {/* row 1 */}
-                        {/* <tr>
-                            <th>1</th>
-                            <td>Cy Ganderton</td>
-                            <td>Quality Control Specialist</td>
-                            <td>Blue</td>
-                        </tr> */}
                         {
-                            loadedUsers.map(user => <tr key={user._id}>
+                            users.map(user => <tr key={user._id}>
                                 <th>1</th>
                                 <td>{user.email}</td>
                                 <td>{user.createdAt}</td>
-                                <td>Blue</td>
+                                <td></td>
+                                <td>
+                                    <button
+                                        onClick={() => handleDelete(user._id)}
+                                        className='btn'>X</button>
+                                </td>
                             </tr>)
                         }
                     </tbody>
